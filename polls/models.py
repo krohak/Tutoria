66b0ed68fa81f987feab72d3bfd324c2b79eb253
Rohak <rohaksinghal14@gmail.com>
2017-09-30 17:04:10 +0800

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    status = models.CharField(max_length=10)
+    items = models.ManyToManyField(Item)
