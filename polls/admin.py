a9f5e8853cfcbec1f0583cccc4a02063209f7497
Rohak <rohaksinghal14@gmail.com>
2017-09-30 17:16:48 +0800

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer
+from .models import Item
+from .models import Cart
+
+admin.site.register(Customer)
+admin.site.register(Item)
+admin.site.register(Cart)
